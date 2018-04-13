c333e7c48163c4648e4b9d5210c3a32e8972eea7
Bilal Haider <ronald.n.mcatee@gmail.com>
2018-04-14 02:16:44 +0500

    methods moved to application helper

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-  include ProjectsHelper
+  include ApplicationHelper
 
   # GET /projects
   # GET /projects.json
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,23 +1,2 @@
 module ProjectsHelper
-  def approved
-    @approved ||= Status.find_by_name("approved")
-  end
-  def disapproved
-    @disapproved ||= Status.find_by_name("disapproved")
-  end
-  def active
-    @active ||= Status.find_by_name("active")
-  end
-  def completed
-    @completed ||= Status.find_by_name("completed")
-  end
-  def fresh
-    @fresh ||= Status.find_by_name("new")
-  end
-  def review
-    @review ||= Status.find_by_name("review")
-  end
-  def payout
-    @payout ||= Status.find_by_name("payout")
-  end
 end
